faac8d521b7a0f3d8337ba33bef5df8e3e1670c8
Ling Guo <ling.guo@rea-group.com>
2022-02-13 13:47:00 +0800

    add size to Hat class

--- part3_advanced/1_classes.rb
+++ part3_advanced/1_classes.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+require '../util'
 ############# class: getter/setter/to_s ###############
 ## method `initialize` will be called when you create a new instance of the class by `.new()`
 ## `@` means it is an instance variable
@@ -36,27 +39,31 @@ puts hat.color
 # green
 
 ############# class: attr_accessor ###############
+print_delimiter('attr_accessor')
 ## attr_reader: only getter
 ## attr_writer: only setter
 ## attr_accessor: both getter and setter
 class Hat2
-  attr_accessor :color
+  attr_accessor :color, :size
 
-  def initialize(color)
+  def initialize(color, size)
     @color = color
+    @size = size
   end
 
   def to_s
-    "This is a hat and the color is: #{@color}"
+    "This is a hat and the color is: #{@color}, the size is #{size}"
   end
 end
 
-hat2 = Hat2.new('blue')
+hat2 = Hat2.new('blue', 10)
 puts hat2
 puts hat2.to_s
 puts hat2.color
+puts hat2.size
 
 ## output
-# This is a hat and the color is: blue
-# This is a hat and the color is: blue
+# This is a hat and the color is: blue, the size is 10
+# This is a hat and the color is: blue, the size is 10
 # blue
+# 10
